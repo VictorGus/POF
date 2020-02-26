74ee306328b59dd764d31ec4a7a1ace21a04f243
VictorGus <retaow@gmail.com>
2020-02-26 23:38:45 +0300

    Display administrative info

--- src/ui/patient_workflow/views.cljs
+++ src/ui/patient_workflow/views.cljs
@@ -10,16 +10,21 @@
    [:#search-input-wrapper
     {:padding-top "15px"
      :padding-left "35px"}
+
     [:#search-btn
      {:margin-right "16px"}]
+
     [:.patient-grid
+
      [:.icon
       {:height "65px"
        :width "65px"
        :margin-top "10px"
        :padding-right "10px"}
+
       [:.icon.img
        {:fill "blue"}]]
+
      [:.patient-record
       {:display "flex"
        :text-decoration "none"
@@ -28,11 +33,34 @@
        :margin-top "12px"
        :border-radius ".25rem"
        :border "1px solid rgba(51, 51, 51, 0.1)"}
+
       [:.patient-info
        {:display "flex"
         :flex-grow "1"
-        :align-items "center"}]]
-     [:.patient-record:hover {:background-color "#fcfdff"}]]]))
+        :align-items "center"}
+
+       [:.patient-address-label
+        #_{:color "white"
+         :background-color "#0069d9"
+         :box-shadow "0 0 2px rgba(0,0,0,0.5)"
+         :padding-right ".5em"
+         :padding-bottom ".2em"
+         :padding-top ".2em"
+         :padding-left ".5em"
+         :border-radius "10rem"}]
+
+       [:.patient-address-value
+        {:padding "5px 5px"}]
+
+       [:.patient-name
+        {:font-size "20px"}]
+       ]]
+
+     [:.right-item
+      {:text-align "right"}]
+
+     [:.patient-record:hover
+      {:background-color "#fcfdff"}]]]))
 
 (defn patient-grid []
   (let [pt-data (rf/subscribe [::model/patient-data])] ;;TODO
@@ -49,10 +77,31 @@
                          "female.svg")}]]
           [:div.patient-info
            [:div
-            [:b (:name item)]
+            [:b.patient-name (:name item)]
             [:span.text-muted.pl-2 (:birthDate item)]
             [:div ;;TODO
-             (:address item)]]]])])))
+             [:span.text-muted
+              "Line:"]
+             [:span.patient-address-value (:address item)]
+             [:span.text-muted
+              "City:"]
+             [:span.patient-address-value (:address item)]
+             [:span.text-muted
+              "State:"]
+             [:span.patient-address-value (:address item)]
+             [:span.text-muted
+              "Country:"]
+             [:span.patient-address-value (:address item)]]]]
+          [:div.right-wrapper
+           [:div.right-item
+            [:span.text-muted
+             "Social Security Number:"]]
+           [:div.right-item
+            [:span.text-muted
+             "Driver License:"]]
+           [:div.right-item
+            [:span.text-muted
+             "Phone:"]]]])])))
 
 (defn search-input []
   (let [input-cnt (r/atom "")

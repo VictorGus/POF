cca098c84e2e5620f425bad398940434d6acd451
VictorGus <retaow@gmail.com>
2020-02-26 18:12:49 +0300

    Patient grid improvement

--- src/ui/patient_workflow/model.cljc
+++ src/ui/patient_workflow/model.cljc
@@ -5,5 +5,5 @@
  ::patient-data
  (fn [db _]
    [{:name "Test Test1" :birthDate "19.11.1960" :address "Test st." :gender "male"}
-    {:name "Test Test2" :birthDate "12.01.1970" :address "Test2 st." :gender "female"}]))
+    {:name "Test Test2" :birthDate "12.01.1970" :address "Test2 st." :gender "female"}])) ;;TODO
 
--- src/ui/patient_workflow/views.cljs
+++ src/ui/patient_workflow/views.cljs
@@ -10,43 +10,75 @@
    [:#search-input-wrapper
     {:padding-top "15px"
      :padding-left "35px"}
+    [:#search-btn
+     {:margin-right "16px"}]
     [:.patient-grid
+     [:.icon
+      {:height "65px"
+       :width "65px"
+       :margin-top "10px"
+       :padding-right "10px"}
+      [:.icon.img
+       {:fill "blue"}]]
      [:.patient-record
       {:display "flex"
-       :padding "12px 12px"
-       :border-bottom "1px solid rgba(51, 51, 51, 0.1)"}
+       :text-decoration "none"
+       :color "black"
+       :padding "8px 8px"
+       :margin-top "12px"
+       :border-radius ".25rem"
+       :border "1px solid rgba(51, 51, 51, 0.1)"}
       [:.patient-info
        {:display "flex"
         :flex-grow "1"
-        :align-items "center"}]]]]))
+        :align-items "center"}]]
+     [:.patient-record:hover {:background-color "#fcfdff"}]]]))
 
 (defn patient-grid []
-  (let [pt-data (rf/subscribe [::model/patient-data])]
+  (let [pt-data (rf/subscribe [::model/patient-data])] ;;TODO
     (fn []
       [:div.patient-grid
        (for [item @pt-data]
          [:a.patient-record
-          [:div.icon ]
+          {:href "#"}
+          [:div.icon
+           [:img {:src (cond
+                         (= (:gender item) "male") ;;TODO
+                         "male.svg"
+                         (= (:gender item) "female")
+                         "female.svg")}]]
           [:div.patient-info
            [:div
             [:b (:name item)]
             [:span.text-muted.pl-2 (:birthDate item)]
-            [:div
+            [:div ;;TODO
              (:address item)]]]])])))
 
 (defn search-input []
-  (let [input-cnt (r/atom "")]
+  (let [input-cnt (r/atom "")
+        dropdown-open? (r/atom false)]
     (fn []
       [:div#search-input-wrapper input-style
        [b/Container
-        {:class "container-md"}
         [b/Row
+         {:styles "height: 48px;"}
          [b/Col
-          {:class "col-md-9"}
+          {:class "col-md-10"}
           [b/Input
            {:type "text"
+            :styles "height: 48px;"
+            :aria-describedby "inputGroup-sizing-sm"
             :placeholder "Search..."
             :on-change #(reset! input-cnt (-> % .-target .-value))}]]
-         [b/Col
-          [b/Button {:color "primary"} "+ Create"]]]
+         [b/Button {:id "search-btn"
+                    :color "outline-primary"} "+ Create"]
+         [b/Dropdown {:isOpen @dropdown-open?
+                      :on-mouse-over #(reset! dropdown-open? true)
+                      :toggle #(swap! dropdown-open? not)
+                      :on-mouse-out  #(reset! dropdown-open? false)}
+          [b/DropdownToggle {:caret true
+                             :color "outline-primary"} "Sort"]
+          [b/DropdownMenu
+           [b/DropdownItem "by birthDate"]
+           [b/DropdownItem "by name"]]]]
         [patient-grid]]])))

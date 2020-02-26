b792cc2b695b67ad38e8da874f42cbb064158f5d
VictorGus <retaow@gmail.com>
2020-02-26 11:11:20 +0300

    Patient grid redone

--- src/ui/patient_workflow/model.cljc
+++ src/ui/patient_workflow/model.cljc
@@ -1,4 +1,9 @@
 (ns ui.patient-workflow.model
   (:require [re-frame.core :as rf]))
 
+(rf/reg-sub
+ ::patient-data
+ (fn [db _]
+   [{:name "Test Test1" :birthDate "19.11.1960" :address "Test st." :gender "male"}
+    {:name "Test Test2" :birthDate "12.01.1970" :address "Test2 st." :gender "female"}]))
 
--- src/ui/patient_workflow/views.cljs
+++ src/ui/patient_workflow/views.cljs
@@ -1,16 +1,38 @@
 (ns ui.patient-workflow.views
   (:require [reagent.core :as r]
+            [re-frame.core :as rf]
             [baking-soda.core :as b]
+            [ui.patient-workflow.model :as model]
             [ui.styles :as styles]))
 
 (def input-style
   (styles/style
    [:#search-input-wrapper
     {:padding-top "15px"
-     :padding-left "35px"}]))
+     :padding-left "35px"}
+    [:.patient-grid
+     [:.patient-record
+      {:display "flex"
+       :padding "12px 12px"
+       :border-bottom "1px solid rgba(51, 51, 51, 0.1)"}
+      [:.patient-info
+       {:display "flex"
+        :flex-grow "1"
+        :align-items "center"}]]]]))
 
-(def patient-record
-  [])
+(defn patient-grid []
+  (let [pt-data (rf/subscribe [::model/patient-data])]
+    (fn []
+      [:div.patient-grid
+       (for [item @pt-data]
+         [:a.patient-record
+          [:div.icon ]
+          [:div.patient-info
+           [:div
+            [:b (:name item)]
+            [:span.text-muted.pl-2 (:birthDate item)]
+            [:div
+             (:address item)]]]])])))
 
 (defn search-input []
   (let [input-cnt (r/atom "")]
@@ -26,4 +48,5 @@
             :placeholder "Search..."
             :on-change #(reset! input-cnt (-> % .-target .-value))}]]
          [b/Col
-          [b/Button {:color "primary"} "+ Create"]]]]])))
+          [b/Button {:color "primary"} "+ Create"]]]
+        [patient-grid]]])))

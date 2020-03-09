efcbf18b8935ad4a0ed942f79e7b03b6c5d635e8
VictorGus <retaow@gmail.com>
2020-03-09 15:56:35 +0300

    Display ssn, dl and phone

--- src/app/core.clj
+++ src/app/core.clj
@@ -11,7 +11,7 @@
   (:import [java.io File]))
 
 (def routes
-  {"patients" {"search" {[:params] {:GET ops/patient-search}}}})
+  {"patients" {"search" {[:params] {:GET ops/patients-search}}}})
 
 (defn handler [{meth :request-method uri :uri :as req}]
   (if-let [res (rm/match [meth uri] routes)]

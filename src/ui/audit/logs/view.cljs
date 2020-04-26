726e651260b8ff1a99744a4359473d2d229f8ef6
VictorGus <retaow@gmail.com>
2020-04-26 16:41:09 +0300

    Init logs grid

--- src/ui/audit/logs/view.cljs
+++ src/ui/audit/logs/view.cljs
@@ -15,7 +15,8 @@
     {:padding-top "15px"
      :padding-left "35px"}]
    [:.not-found {:font-size "22px"}]
-   [:.filter-form {:z-index "999"}]))
+   [:.filter-form {:z-index "999"
+                   :background-color "white"}]))
 
 (defn filter-form [close-fn]
   [:div.container.border.border-top-0.rounded.filter-form
@@ -51,29 +52,45 @@
   (let [show-form? (r/atom false)
         close-fn #(reset! show-form? false)]
     (fn []
-      [:div#search-input-wrapper input-style
-       [:div.container
-        [:div.row
-         {:styles "height: 48px;"}
-         [:div.subform.col-md-12
-          {:tab-index 0
-           :on-focus #(reset! show-form? true)
-           :on-blur close-fn}
-          [b/Input
-           {:type "text"
-            :styles "height: 48px;"
-            :placeholder "Search..."
-            :on-change (fn [e]
-                         (let [v (-> e .-target .-value)]
-                           (js/setTimeout (fn []
-                                            (println v))
-                                          700)))}]
-          (when @show-form?
-            [filter-form close-fn])]]]])))
+      [:div.row
+       {:styles "height: 48px;"}
+       [:div.subform.col-md-12
+        {:tab-index 0
+         :on-focus #(reset! show-form? true)
+         :on-blur close-fn}
+        [b/Input
+         {:type "text"
+          :styles "height: 48px;"
+          :placeholder "Search..."
+          :on-change (fn [e]
+                       (let [v (-> e .-target .-value)]
+                         (js/setTimeout (fn []
+                                          (println v))
+                                        700)))}]
+        (when @show-form?
+          [filter-form close-fn])]])))
 
 (defn logs-grid []
   (fn []
-    [input-form]))
+    [:div#search-input-wrapper input-style
+     [:div.container
+      [input-form]
+      [:table.table.table-hover
+       [:thead
+        [:tr
+         [:th {:scope "col"} "Status"]
+         [:th {:scope "col"} "TS"]
+         [:th {:scope "col"} "Display"]
+         [:th {:scope "col"} [:span "Duration"
+                              [:i.fa.fa-sort.ml-1 {:aria-hidden "true"
+                                                   :style {:cursor "pointer"}
+                                                   :on-click #(println "TODO")}]]]]]
+       [:tbody
+        [:tr
+         [:td [:span.badge.badge-success "200"]]
+         [:td "2019-02-13"]
+         [:td "Viewing patient Anna13 Marina14 (DL:88080)"]
+         [:td "145 ms"]]]]]]))
 
 (pages/reg-subs-page
  model/logs

9f1fd169b48f57310f9fe83eb1620cef967ca8bb
VictorGus <retaow@gmail.com>
2020-05-08 02:04:37 +0300

    Add user entity

--- init/init.sql
+++ init/init.sql
@@ -1,3 +1,5 @@
 SELECT 'CREATE DATABASE testbase TEMPLATE fhirbase' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'testbase')\gexec
 
 CREATE EXTENSION IF NOT EXISTS jsquery
+
+CREATE table IF NOT EXISTS public_user as (select * from patient) with no data
